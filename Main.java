76cad210db53acf4b19cb48ffacb2b44310d51a7
Allann777 <allandamaceno65@gmail.com>
2026-05-05 20:10:17 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite seu nome completo: ");
+        String nome = entrada.nextLine();
+
+        System.out.print("Digite seu cargo: ");
+        String cargo = entrada.nextLine();
+
+        System.out.print("Nivel de acesso (1 a 10): ");
+        int acesso = entrada.nextInt();
+
+        System.out.print("Nivel de sigilo (1 a 10): ");
+        int sigilo = entrada.nextInt();
+
+        if (acesso < 1 || acesso > 10 || sigilo < 1 || sigilo > 10) {
+            System.out.println("Valores invalidos");
+        } else if (acesso >= sigilo) {
+            System.out.println("ACESSO PERMITIDO para " + nome);
+        } else {
+            System.out.println("ACESSO NEGADO para " + nome);
+        }
+    }
+}
+
+    
